ab554ab49d962bbe584614417b939c22ffd669b4
tima2015 <woodgoldfilm@gmail.com>
2026-08-01 03:23:04 +0500

    feat: implement MessageHistory service

---
+++ src/main/java/com/github/tima2015/chatparrot/server/service/MessageHistory.java
@@ -0,0 +1,64 @@
+package com.github.tima2015.chatparrot.server.service;
+
+import com.github.tima2015.chatparrot.server.data.Message;
+import jakarta.annotation.PreDestroy;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+@Slf4j
+public class MessageHistory {
+    private final ConcurrentLinkedQueue<Message> history = new ConcurrentLinkedQueue<>();
+    private final Set<MessageWriter> writers;
+    @Getter
+    @Setter
+    private volatile int flushThreshold;
+    private final AtomicInteger count = new AtomicInteger(0);
+
+    @Autowired
+    public MessageHistory(Set<MessageWriter> writers,  @Value("${history-writer.flush-threshold:100}") int flushThreshold) {
+        this.writers = writers;
+        this.flushThreshold = flushThreshold;
+    }
+
+    public void receive(Message message) {
+        log.debug("message receive");
+        history.add(message);
+        if (count.incrementAndGet() >= flushThreshold) {
+            flush();
+        }
+    }
+
+    @PreDestroy
+    @Scheduled(fixedDelayString = "${history-writer.flush-schedule:10000}")
+    public synchronized void flush() {
+        log.debug("flush() start");
+        List<Message> tmp = new ArrayList<>();
+        Message m;
+        while ((m = history.poll()) != null) {
+            tmp.add(m);
+        }
+        if (tmp.isEmpty()) {
+            return;
+        }
+        tmp = Collections.unmodifiableList(tmp);
+        for (MessageWriter writer : writers) {
+            try {
+                writer.write(tmp);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+        count.addAndGet(-tmp.size());
+        log.debug("flush() done");
+    }
+}
