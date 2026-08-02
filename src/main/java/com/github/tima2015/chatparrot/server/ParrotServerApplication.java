ea2b81617d9741caba5fc1e4ca3a7419a0e7018b
tima2015 <woodgoldfilm@gmail.com>
2026-08-02 21:25:04 +0500

    feat: create main class

---
+++ src/main/java/com/github/tima2015/chatparrot/server/ParrotServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.tima2015.chatparrot.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParrotServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParrotServerApplication.class, args);
+    }
+
+}
