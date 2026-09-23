7867ad0daea1183f8bd874d9ebfa5579577d5593
tima2015 <woodgoldfilm@gmail.com>
2026-09-23 23:21:01 +0500

    test: change chatBetweenTwoPlayersShouldSucceed test logic

--- src/test/java/com/github/tima2015/chatparrot/server/WebSocketIntegrationTest.java
+++ src/test/java/com/github/tima2015/chatparrot/server/WebSocketIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 import org.springframework.web.reactive.socket.client.WebSocketClient;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 import reactor.test.StepVerifier;
 import tools.jackson.databind.ObjectMapper;
 
@@ -21,12 +22,13 @@ public class WebSocketIntegrationTest {
     private int port;
 
     private WebSocketClient client;
-    URI uri = URI.create("ws://localhost:" + port + "/ws?token=super-secret-parrot-token");
+    private URI uri;
 
 
     @BeforeEach
     void setUp() {
         client = new ReactorNettyWebSocketClient();
+        uri = URI.create("ws://localhost:" + port + "/ws?token=super-secret-parrot-token");
     }
 
     @Test
@@ -41,7 +43,8 @@ public class WebSocketIntegrationTest {
 
     @Test
     void chatBetweenTwoPlayersShouldSucceed() {
-        WebSocketClient another = new ReactorNettyWebSocketClient();
+        WebSocketClient client1 = new ReactorNettyWebSocketClient();
+        WebSocketClient client2 = new ReactorNettyWebSocketClient();
 
         String author0 = "Player 1";
         String content0 = "Hello World!";
@@ -49,39 +52,47 @@ public class WebSocketIntegrationTest {
         String content1 = "Hello ParrotServer!";
 
         String jsonTemplate = """
-            {
-              "author": "%s",
-              "content": "%s"
-            }""";
+        {
+          "author": "%s",
+          "content": "%s"
+        }""";
 
-        StepVerifier.create(
-                client.execute(uri, session -> {
-                    Mono<Void> send = session.send(Mono.just(session.textMessage(jsonTemplate.formatted(author0, content0))));
+        Sinks.One<Void> client1Ready = Sinks.one();
+        Sinks.One<Void> client2Ready = Sinks.one();
 
-                    Mono<Void> receive = session.receive()
-                            .map(WebSocketMessage::getPayloadAsText)
-                            .map(this::asMessage) // парсим обратно в объект
-                            .filter(m -> m.getAuthor().equals(author1 ))
-                            .take(1)
-                            .then();
+        Mono<Void> session1 = client1.execute(uri, session -> {
+            client1Ready.tryEmitEmpty();
 
-                    return Mono.zip(send, receive).then();
-                })
-        ).expectComplete().verify(Duration.ofSeconds(5));
-
-        StepVerifier.create(
-                another.execute(uri, session -> {
-                    Mono<Void> send = session.send(Mono.just(session.textMessage(jsonTemplate.formatted(author1, content1))));
-
-                    Mono<Void> receive = session.receive()
+            return Mono.when(client1Ready.asMono(), client2Ready.asMono())
+                    .then(session.send(Mono.just(session.textMessage(
+                            jsonTemplate.formatted(author0, content0)))))
+                    .and(session.receive()
                             .map(WebSocketMessage::getPayloadAsText)
                             .map(this::asMessage)
-                            .filter(m -> m.getAuthor().equals(author0))
+                            .filter(m -> m.getAuthor().equals(author1)
+                                    && m.getContent().equals(content1))
                             .take(1)
-                            .then();
-                    return Mono.zip(send, receive).then();
-                })
-        ).expectComplete().verify(Duration.ofSeconds(5));
+                            .then());
+        });
+
+        Mono<Void> session2 = client2.execute(uri, session -> {
+            client2Ready.tryEmitEmpty();
+
+            return Mono.when(client1Ready.asMono(), client2Ready.asMono())
+                    .then(session.send(Mono.just(session.textMessage(
+                            jsonTemplate.formatted(author1, content1)))))
+                    .and(session.receive()
+                            .map(WebSocketMessage::getPayloadAsText)
+                            .map(this::asMessage)
+                            .filter(m -> m.getAuthor().equals(author0)
+                                    && m.getContent().equals(content0))
+                            .take(1)
+                            .then());
+        });
+
+        StepVerifier.create(Mono.when(session1, session2))
+                .expectComplete()
+                .verify(Duration.ofSeconds(5));
     }
 
     private Message asMessage(String json) {
