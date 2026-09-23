4946092f7b04ad8eff52b0f7fd52bd447592b35f
tima2015 <woodgoldfilm@gmail.com>
2026-09-23 23:25:15 +0500

    feat: improve WebSocketConfig class - make config public - replace HashMap with Map.of()

--- src/main/java/com/github/tima2015/chatparrot/server/config/WebSocketConfig.java
+++ src/main/java/com/github/tima2015/chatparrot/server/config/WebSocketConfig.java
@@ -5,20 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
-import org.springframework.web.reactive.socket.WebSocketHandler;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-class WebSocketConfig {
+public class WebSocketConfig {
 
     @Bean
-    public HandlerMapping handlerMapping() {
-        Map<String, WebSocketHandler> map = new HashMap<>();
-        map.put("/ws", new ParrotWebSocketHandler());
+    public HandlerMapping handlerMapping(ParrotWebSocketHandler handler) {
         int order = -1;
-
-        return new SimpleUrlHandlerMapping(map, order);
+        return new SimpleUrlHandlerMapping(Map.of("/ws", handler), order);
     }
 }
